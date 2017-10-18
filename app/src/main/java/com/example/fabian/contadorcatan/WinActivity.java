95fa93c5933c0019047a82b12717b002ca2b9219
Fabian <20464654j@iespoblenou.org>
2017-10-18 11:24:38 +0200

    Obtenemos el bundle en el segundo activity y extraemos el numero de jugador que hemos pasado

--- app/src/main/java/com/example/fabian/contadorcatan/WinActivity.java
+++ app/src/main/java/com/example/fabian/contadorcatan/WinActivity.java
@@ -5,9 +5,19 @@ import android.os.Bundle;
 
 public class WinActivity extends AppCompatActivity {
 
+    private int winner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
+
+        // obtenemos el bundle
+        Bundle b = getIntent().getExtras();
+        // si este no es null
+        if( b != null ){
+            // obtiene el valor y lo guarda en la variable designada para el numero de jugador ganador
+            winner = b.getInt( "winner" );
+        }
     }
 }
