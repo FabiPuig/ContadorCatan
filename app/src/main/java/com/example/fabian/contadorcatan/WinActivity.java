8f4256ebff55c8776412add9da805d45f1adeba6
Fabian <20464654j@iespoblenou.org>
2017-10-18 13:31:26 +0200

    Hacemos que la imagen se vea detras de la barra de estado del dispositivo. Para ello es necesario que la barra de estado sea transparente ( Paso anterior )

--- app/src/main/java/com/example/fabian/contadorcatan/WinActivity.java
+++ app/src/main/java/com/example/fabian/contadorcatan/WinActivity.java
@@ -2,6 +2,7 @@ package com.example.fabian.contadorcatan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class WinActivity extends AppCompatActivity {
@@ -13,6 +14,10 @@ public class WinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         ivWinner = (ImageView) findViewById( R.id.iv_winner );
 
